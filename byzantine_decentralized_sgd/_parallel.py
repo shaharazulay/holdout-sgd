949adbafa740fd0d438338b08e0a83b0f4c66d31
shaharazulay <shahar4@gmail.com>
2019-09-23 10:05:57 +0300

    removed python2.0 corrections

--- byzantine_decentralized_sgd/_parallel.py
+++ byzantine_decentralized_sgd/_parallel.py
@@ -4,24 +4,7 @@ import progressbar
 
 
 def _create_pool():
-    #return mp.Pool(mp.cpu_count())
-    
-    ###
-    import sys
-    # For python 2/3 compatibility, define pool context manager
-    # to support the 'with' statement in Python 2
-    if sys.version_info[0] == 2:
-        from contextlib import contextmanager
-        @contextmanager
-        def multiprocessing_context(*args, **kwargs):
-            pool = mp.Pool(*args, **kwargs)
-            yield pool
-            pool.terminate()
-    else:
-        multiprocessing_context = mp.Pool
-        
-    return multiprocessing_context
-    ###
+    return mp.Pool(mp.cpu_count())
     
 def run_in_parallel(func, args_list):    
     pool = _create_pool()
@@ -37,15 +20,14 @@ def async_run_in_parallel(func, args_list):
     
     results = []
     
-    #pool = _create_pool()
-    with _create_pool()(mp.cpu_count()) as pool:
-        results_obj = [pool.apply_async(func, args, callback=results.append) for args in args_list]
-        
-        while len(results) != len(args_list):
-            pbar.update(len(results))
-            time.sleep(0.5)
+    pool = _create_pool()
+    results_obj = [pool.apply_async(func, args, callback=results.append) for args in args_list]
+    
+    while len(results) != len(args_list):
+        pbar.update(len(results))
+        time.sleep(0.5)
     
     pbar.finish()
-    #pool.close()
+    pool.close()
     return results
     
